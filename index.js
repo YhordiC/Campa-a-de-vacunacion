6ff713952a2aa3a267be38223457d2a0ac3d9314
Yhordi Choque Espinoza <yordychoque124@gmail.com>
2023-02-27 16:44:06 -0400

    añadi un menu para el nav solo para telefonos

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const parafo= document.querySelectorAll('.parafo-inf');
 const triangulo= document.querySelectorAll('.triangulo2');
+const listaHeader= document.querySelector('.container-nav');
+const Close= document.getElementById('close');
+const menu = document.getElementById('menu');
+
+menu.addEventListener('click',()=>{
+  listaHeader.classList.add('lista')
+  menu.style.display='none';
+  Close.style.display='block';
+
+})
+Close.addEventListener('click',()=>{
+  listaHeader.classList.remove('lista');
+  menu.style.display='block';
+  Close.style.display='none';
+})
 
 triangulo.forEach(tr=>{
     tr.addEventListener('click',()=>{
       tr.classList.toggle('logo')
     })
-}
-    )
+} )
+
